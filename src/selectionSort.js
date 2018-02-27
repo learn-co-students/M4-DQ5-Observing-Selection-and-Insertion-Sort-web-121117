e17aef29bb65ead8266f855b99c061da06235ce9
olegchursin <olegchursin@gmail.com>
2018-02-27 09:56:21 -0500

    Done.

--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,20 @@
+
 function selectionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+	var length = arr.length;
+	for (var i = 0; i < length - 1; i++) {
+		//Number of passes
+		var min = i; //min holds the current minimum number position for each pass; i holds the Initial min number
+		for (var j = i + 1; j < length; j++) { //Note that j = i + 1 as we only need to go through unsorted array
+			if (arr[j] < arr[min]) { //Compare the numbers
+				min = j; //Change the current min number position if a smaller num is found
+			}
+		}
+		if (min != i) {
+			//After each pass, if the current min num != initial min num, exchange the position.
+			//Swap the numbers
+			var tmp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = tmp;
+		}
+	}
 }
